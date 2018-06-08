a1ef68b2781cd0bea80b3cfadf8dc41f186672ad
Brent McManus <brent.mcmanus@me.com>
2018-06-07 20:24:50 -0700

    added initial main for class testing

--- svm.py
+++ svm.py
@@ -5,4 +5,17 @@ Created on Thu Jun  7 19:12:30 2018
 
 @author: Brent Mcmanus
 """
+import pandas as pd
 
+if __name__ == "__main__":
+    trainSet = pd.read_csv('train.csv')
+    testSet = pd.read_csv('test.csv')
+    
+    #replace male/female with a binary classification
+    trainSet['Sex'] = trainSet['Sex'].map({'female': 1, 'male': 0}).astype(int)
+    testSet['Sex'] = testSet['Sex'].map({'female': 1, 'male': 0}).astype(int)
+    
+    #grab labels and drop from the features
+    trainTarg = trainSet["Survived"]
+    trainSet = trainSet.drop("Survived", axis=1)
+    
